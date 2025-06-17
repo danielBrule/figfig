c70c531d32d2cd82b8e50b18ac4939388646f73b
DanielBRULE <d.brule.ext@sportradar.com>
2025-06-17 16:39:47 +0100

    fixing dev.yml

--- exec.cmd
+++ exec.cmd
@@ -12,9 +12,9 @@ terraform init -backend-config="envs/dev/backend.tf"
 terraform plan -var-file="envs/dev/terraform.tfvars"
 terraform apply -var-file="envs/dev/terraform.tfvars" -auto-approve
 
-
-REM cd terraform/envs/prod
-REM terraform destroy -auto-approve; terraform init; terraform apply -auto-approve
+REM terraform init -backend-config="envs/prod/backend.tf" -reconfigure
+REM terraform plan -var-file="envs/prod/terraform.tfvars"
+REM terraform apply -var-file="envs/prod/terraform.tfvars" -auto-approve
 
 
 cd ..
@@ -25,7 +25,6 @@ REM docker build --build-arg ENV=prod -t figfigacr.azurecr.io/figfig-app:latest 
 
 
 docker push figfigacrdev.azurecr.io/figfig-app:dev
-
 REM az keyvault set-policy --name figfig-key-vault-dev --spn 5809b5c3-c9f6-466b-a200-7e3e03d7ef5b --secret-permissions get list
 
 docker run --env-file .env.dev figfigacrdev.azurecr.io/figfig-app:dev
@@ -41,3 +40,6 @@ REM py -3.13 -m venv venv
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
 ./venv\Scripts\activate
 REM pip install -r requirements.txt
+
+
+REM AZURE_SUBSCRIPTION_ID: az account show --query id -o tsv
