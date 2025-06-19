2ce769f82fae97efe806036e0956c089ce0df9be
DanielBRULE <d.brule.ext@sportradar.com>
2025-06-19 17:34:27 +0100

    test

--- terraform/envs/prod/backend.tf
+++ terraform/envs/prod/backend.tf
@@ -1,4 +1,8 @@
-resource_group_name  = "figfig-tfstate-rg"
-storage_account_name = "figfigtfstateprod"
-container_name       = "tfstate"
-key                  = "prod.terraform.tfstate"
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "figfig-tfstate-rg"
+    storage_account_name = "figfigtfstateprod"
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"
+  }
+}
