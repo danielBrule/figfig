f953470675b0c0317108d023a5dd79a074ff9b91
DanielBRULE <d.brule.ext@sportradar.com>
2025-06-23 23:13:26 +0100

    rahhhhhhh

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,8 @@ provider "azurerm" {
   features {}
   subscription_id = var.subscription_id
   tenant_id       = var.tenant_id
-  use_oidc = true
+  client_id       = var.client_id
+  client_secret   = var.client_secret
 }
 
 provider "azuread" {}
