02e56c1986a771fd542467814b78244986a8cc16
DanielBRULE <d.brule.ext@sportradar.com>
2025-06-30 22:46:26 +0100

    trying to allow container to access sql server

--- terraform/tf_sql/main.tf
+++ terraform/tf_sql/main.tf
@@ -28,3 +28,10 @@ resource "azurerm_mssql_firewall_rule" "allow_azure_office" {
   start_ip_address = "212.36.188.0"
   end_ip_address   = "212.36.188.255"
 }
+
+resource "azurerm_mssql_firewall_rule" "allow_azure_services" {
+  name             = "AllowAzureServices"
+  server_id        = azurerm_mssql_server.example.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
+}
