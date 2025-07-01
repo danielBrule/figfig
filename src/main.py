7d0bd0e84160bde9ddb33bcd6b2a58a84e7e39cb
DanielBRULE <d.brule.ext@sportradar.com>
2025-07-01 23:07:53 +0100

    test

--- src/main.py
+++ src/main.py
@@ -4,12 +4,12 @@ import subprocess
 SCRAPER = os.getenv("SCRAPER")
 
 if SCRAPER == "scraper-dailyurl":
-    subprocess.run(["python", "src/scrapers/scrap_1_get_daily_urls.py"])
+    subprocess.run(["python", "scrapers/scrap_1_get_daily_urls.py"])
 elif SCRAPER == "scraper-articles-primary-info":
-    subprocess.run(["python", "src/scrapers/scrap_2_get_articles_primary_info.py"])
+    subprocess.run(["python", "scrapers/scrap_2_get_articles_primary_info.py"])
 elif SCRAPER == "scraper-articles-secondary-info":
-    subprocess.run(["python", "src/scrapers/scrap_3_get_articles_secondary_info.py"])
+    subprocess.run(["python", "scrapers/scrap_3_get_articles_secondary_info.py"])
 elif SCRAPER == "scraper-comments":
-    subprocess.run(["python", "src/scrapers/scrap_4_get_comments.py"])
+    subprocess.run(["python", "scrapers/scrap_4_get_comments.py"])
 else:
     raise ValueError(f"Unknown SCRAPER type: {SCRAPER}")
