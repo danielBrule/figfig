7ebc562e198c5f0dda63e5ddd40a6207b2eadef9
DanielBRULE <d.brule.ext@sportradar.com>
2025-07-02 08:59:50 +0100

    fixingenv issue

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 set -e
 
+if [ -f .env ]; then
+  echo "Loading environment variables from .env"
+  export $(grep -v '^#' .env | xargs)
+fi
+
+
 if ! python -c "from sqlalchemy import inspect; from db.database import engine; print(inspect(engine).has_table('newspaper'))" | grep True; then
   echo "Tables missing, running init_db.py..."
   python ./init_db.py
